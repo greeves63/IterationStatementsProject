82a62da6b9d9409d6958010f6701d73827850e60
Gregory Reeves Jr <gregoryreevesjr@gmail.com>
2020-06-23 15:00:15 -0500

    Finished updating the IterationStatements again

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -10,32 +10,49 @@ namespace IterationStatements
             //TODO
 
             //Create a List called "numbers"
+            List<int> numbers = new List<int>();
+
             //Create a variable of type int with an initializer of 0
 
 
 
+            var index = 0;
+
             // Create a do-while loop
+          
+            do
             {
                 // Increment your variable by 1
+                index++;
                 // Then add your variable to "numbers"
+                numbers.Add(index);
             }
             // While your variable is less than 100
-
+            while (index < 100);
 
 
             // Create a while loop
             // While your variable is less than 200
+            while (index < 200)
             {
                 // Increment your variable by 1
+                index += 1;
                 // Then add your variable to "numbers"
+                numbers.Add(index);
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
             // Write your variable to the console
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+                
+            }
 
-            Console.WriteLine("");
+            Console.WriteLine(""); //creates empty space
+
             Console.WriteLine("Decrease:");
 
             // Create a for loop
@@ -43,8 +60,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for(int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine($"The number {numbers[i]} is at the {i} index.");
             }
         }
     }
